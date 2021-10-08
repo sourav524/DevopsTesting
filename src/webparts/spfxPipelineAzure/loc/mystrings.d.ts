02bedc89d28689597295fc78f3abc6adb2c32f72
Sourav <rudrasisodiya786@gmail.com>
2021-10-08 15:34:01 +0530

    Initial commit

---
+++ src/webparts/spfxPipelineAzure/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpfxPipelineAzureWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpfxPipelineAzureWebPartStrings' {
+  const strings: ISpfxPipelineAzureWebPartStrings;
+  export = strings;
+}
